bd0d691893134e7bf8f4266f304a1a85ed1f5a02
ryanv <ryan.vermeille@divcom.ch>
2025-04-10 09:22:25 +0200

    Add method modifyAllocataire

--- src/main/java/ch/hearc/cafheg/business/allocations/AllocataireService.java
+++ src/main/java/ch/hearc/cafheg/business/allocations/AllocataireService.java
@@ -1,4 +1,9 @@
 package ch.hearc.cafheg.business.allocations;
 
 public class AllocataireService {
+
+    public void modifyAllocataire (String NoAVS, String newFirstName, String newLastName) {
+
+    }
+
 }
