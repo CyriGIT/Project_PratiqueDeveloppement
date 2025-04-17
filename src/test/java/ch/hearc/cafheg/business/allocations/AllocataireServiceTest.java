135005937c3a52eb15ba4cec977ac747fdca76ac
CyriGIT <cyrille.dosghali@he-arc.ch>
2025-04-17 09:11:57 +0200

    Added test for modify allocataire in AllocataireServiceTest.java

---
+++ src/test/java/ch/hearc/cafheg/business/allocations/AllocataireServiceTest.java
@@ -0,0 +1,39 @@
+package ch.hearc.cafheg.business.allocations;
+
+import ch.hearc.cafheg.infrastructure.persistance.AllocataireMapper;
+import ch.hearc.cafheg.infrastructure.persistance.AllocationMapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+class AllocataireServiceTest {
+    private AllocataireService allocataireService;
+
+    private AllocataireMapper allocataireMapper;
+    private AllocationMapper allocationMapper;
+
+    @BeforeEach
+    void setUp() {
+        allocataireMapper = Mockito.mock(AllocataireMapper.class);
+        allocationMapper = Mockito.mock(AllocationMapper.class);
+
+        allocataireService = new AllocataireService(allocataireMapper);
+    }
+
+    @Test
+    @DisplayName("modifier un allocataire : si le nom ou le prénom est modifié, l'allocataire est modifié")
+    void modifyAllocataire_IfNameOrFirstNameModified_ShouldModifyAllocataire() {
+        Allocataire allocataire = new Allocataire(new NoAVS("1000-2000"), "Geiser", "Arnaud");
+        Mockito.when(allocataireMapper.findAll(null)).thenReturn(List.of(allocataire));
+
+        Allocataire newAllocataire = new Allocataire(new NoAVS("1000-2000"), "Geiser", "Aurélie");
+        allocataireService.modifyAllocataire(newAllocataire);
+
+        Mockito.verify(allocataireMapper).updateAllocataire(newAllocataire);
+    }
+
+}
