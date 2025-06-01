0bf2679b137a1445f0f8bc0bb998bec7f9138e5a
smartini <saverio.martini@he-arc.ch>
2025-06-01 18:56:45 +0200

    delete log4j-to-slf4j from lib and add log4j-core to allow create log files, modify log4j2.xml and his tests to make it pass; it works on my machine cit.

--- src/test/java/ch/hearc/cafheg/business/logs/Log4jTest.java
+++ src/test/java/ch/hearc/cafheg/business/logs/Log4jTest.java
@@ -4,9 +4,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Log4jTest {
-    private static final Logger logger = LogManager.getLogger("ch.hearc.cafheg.business");
+    private static final Logger logger = LogManager.getLogger(Log4jTest.class);
+
 
     public static void main(String[] args) {
+        System.setProperty("log4j2.debug", "true");
+
         // Messages pour tester l'écriture dans les fichiers de log
         logger.debug("Message de débogage : écrit dans la console uniquement");
         logger.info("Message d'information : écrit dans cafheg_{date-jour}.log");
--- src/test/java/ch/hearc/cafheg/business/logs/LogFileTest.java
+++ src/test/java/ch/hearc/cafheg/business/logs/LogFileTest.java
@@ -5,9 +5,14 @@ import java.io.File;
 public class LogFileTest {
     public static void main(String[] args) {
         File errorLog = new File("logs/err.log");
-        File serviceLog = new File("logs/cafheg_" + java.time.LocalDate.now() + ".log");
+        File serviceLog = new File("logs/cafheg.log");
+
+
+        System.out.println("err.log path : " + errorLog.getAbsolutePath());
+        System.out.println("cafheg log path : " + serviceLog.getAbsolutePath());
 
         System.out.println("Fichier err.log existe : " + errorLog.exists());
-        System.out.println("Fichier cafheg_{date-jour}.log existe : " + serviceLog.exists());
+        System.out.println("Fichier cafheg log existe : " + serviceLog.exists());
+
     }
 }
